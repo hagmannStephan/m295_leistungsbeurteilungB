f7c216e5ad0c87416da007b94569b5f986d48024
Stephan <stephanhagmann@outlook.com>
2023-12-20 08:56:34 +0100

    CREATE THE BASIC PROJECT STRUCUTRE \n \n The project strucutre is essential for a working project. I create a .gitignore, added some information in the readme and already wrote some basic js code

---
+++ task.js
@@ -0,0 +1,15 @@
+// Git repo: https://github.com/hagmannStephan/m295_leistungsbeurteilungB.git
+
+const express = require('express');
+
+let tasks = [
+    {
+        "id": 1,
+        "title": "finish work",
+        "description": "finish the current practical exam",
+        "done": false,
+        "dueDate": "2023-12-20T15:30:00.871Z",
+        "createdDate": new Date
+    }
+]
+
