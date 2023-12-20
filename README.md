0df3751dc55fd881b849820948c4c7028819eb0a
Stephan <stephanhagmann@outlook.com>
2023-12-20 10:15:07 +0100

    ADD THE BASE ROUTES
    I created routes to create, update, delete and read entries (CRUD). These are the base requirements for this project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # m295_leistungsbeurteilungB
+## Running the application
+1. Run the following commands in your project root directory: `npm install express`, `npm install express-session`
--- task.js
+++ task.js
@@ -1,7 +1,23 @@
 // Git repo: https://github.com/hagmannStephan/m295_leistungsbeurteilungB.git
 
-const express = require('express');
+const express = require('express');     // Setup the app
+const app = express();
 
+const session = require('express-session');     // Declare session
+const { request } = require('http');
+
+app.use(express.json());        // Enabels parsing of incoming json
+app.use(session({               // Set up a session management
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+
+// Port, where the application is running
+const port = 3000;
+
+// List of the tasks
 let tasks = [
     {
         "id": 1,
@@ -10,6 +26,128 @@ let tasks = [
         "done": false,
         "dueDate": "2023-12-20T15:30:00.871Z",
         "createdDate": new Date
+    },
+    {
+        "id": 2,
+        "title": "play guitar",
+        "description": "learn a new song to play on the guitar",
+        "done": false,
+        "dueDate": "2023-12-24T18:00:00.871Z",
+        "createdDate": new Date
     }
 ]
 
+// Query every task
+app.get('/tasks', (request, response) => {
+    return response.status(200).json(tasks);
+})
+
+// Create a new task and return that task (if successful)
+app.post('/tasks', (request, response) => {
+    const id = tasks[tasks.length - 1].id + 1;
+    const title = request.body.title;
+    const description = request.body.description;
+    let done = request.body.done;
+    let dueDate = request.body.dueDate;
+    const createdDate = new Date;
+
+    if (!done) {        // Set done to false by standard
+        done = false;
+    }
+
+    if (!dueDate) {     // If no dueDate is provided, there will be no due date
+        dueDate = "";
+    }
+
+    if (!title || !description) {   // Check if required values are set
+        return response.status(422).send();
+    }
+
+    const task = {          // Create the new task object
+        "id": id,
+        "title": title,
+        "description": description,
+        "done": done,
+        "dueDate": dueDate,
+        "createdDate": createdDate
+    }
+
+    tasks.push(task);       // Add object to tasks
+
+    return response.status(201).json(task);
+})
+
+// Query for a specific task by id
+app.get('/tasks/:id', (request, response) => {
+    const id = parseInt(request.params.id); // Get the provided id
+    let task = tasks.findIndex((task) => task.id === id);
+    task = tasks[task];                      // Get the corresponding object
+
+    if(!task) {                             // Check if a task got found
+        return response.status(404).send();
+    }
+
+    return response.status(200).json(task);
+})
+
+app.put('/tasks/:id', (request, response) => {
+    const id = parseInt(request.params.id); // Get the provided id
+    let task = tasks.findIndex((task) => task.id === id);
+    task = tasks[task];                      // Get the corresponding object
+
+    if(!task) {                             // Check if a task got found
+        return response.status(404).send();
+    }
+
+    let title = request.body.title;       // Get the provided values
+    let description = request.body.description;
+    let done = request.body.done;
+    let dueDate = request.body.dueDate;
+
+    if (!title) {                       // If not provided, do not update the field
+        title = task.title;
+    }
+
+    if (!description) {
+        description = task.description;
+    }
+
+    if(!done) {
+        done = task.done;
+    }
+
+    if(!dueDate) {
+        dueDate = task.dueDate;
+    }
+
+    let updated_task = {            // Create the updated task-object
+        "id": task.id,
+        "title": title,
+        "description": description,
+        "done": done,
+        "dueDate": dueDate,
+        "createdDate": task.createdDate
+    }
+
+    task = updated_task;        // Modify the existing task to the updated task
+
+    return response.status(200).json(updated_task);
+})
+
+app.delete('/tasks/:id', (request, response) => {
+    const id = parseInt(request.params.id); // Get the provided id
+    let taskIndex = tasks.findIndex((task) => task.id === id);
+    let task = tasks[taskIndex];                      // Get the corresponding object
+
+    if(!task) {                             // Check if a task got found
+        return response.status(404).send();
+    }
+
+    tasks.splice(taskIndex, 1);             // Remove the entry from the list
+
+    return response.status(200).send();
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
